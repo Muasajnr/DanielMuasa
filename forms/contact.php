800ae8ce3123de760f39992533d427a4027cb234
Muasajnr <116666398+Muasajnr@users.noreply.github.com>
2024-01-25 19:26:03 +0300

    Initial commit

--- forms/contact.php
+++ forms/contact.php
@@ -1,35 +1,39 @@
 <?php
-  
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $receiving_email_address = 'elmmwas7@gmail.com';
 
-  $receiving_email_address = 'elmmwas7@gmail.com';
+    if (file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php')) {
+        include($php_email_form);
+    } else {
+        die('Unable to load the "PHP Email Form" Library!');
+    }
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+    $contact = new php_email_form;
+    
+    $contact->ajax = true;
 
-  
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+    $contact->to = $receiving_email_address;
+    $contact->from_name = isset($_POST['name']) ? $_POST['name'] : '';
+    $contact->from_email = isset($_POST['email']) ? $_POST['email'] : '';
+    $contact->subject = isset($_POST['subject']) ? $_POST['subject'] : '';
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+    $contact->smtp = array(
+      'host' => 'smtp-relay.brevo.com',
+      'username' => 'elmmwas7@gmail.com',
+      'password' => '3f1ZsXQ7Acx9dWM4',
+      'port' => '587'
+    );
 
-  echo $contact->send();
-?>
+    $contact->add_message(isset($_POST['name']) ? $_POST['name'] : '', 'From');
+    $contact->add_message(isset($_POST['email']) ? $_POST['email'] : '', 'Email');
+    $contact->add_message(isset($_POST['message']) ? $_POST['message'] : '', 'Message', 10);
+
+    echo $contact->send();
+} else {
+    http_response_code(405); 
+    exit('Method Not Allowed');
+}
+
+
+
